81d4bb18d5baf9083f7cad923d5bb02e87581cff
astra-kawa <mjagodzski@gmail.com>
2026-07-18 20:03:06 -0400

    initial lru cache implementation (it sucks)

--- src/problems/lru_cache.rs
+++ src/problems/lru_cache.rs
@@ -1,3 +1,78 @@
 // source: https://leetcode.com/problems/lru-cache
 
-pub fn run() {}
+use std::collections::HashMap;
+
+struct LRUCache {
+    capacity: i32,
+    items: HashMap<i32, i32>,
+    // could use a fixed array but would need to make the cache a generic
+    order: Vec<i32>,
+}
+
+impl LRUCache {
+    fn new(capacity: i32) -> Self {
+        LRUCache {
+            capacity,
+            items: HashMap::new(),
+            order: Vec::new(),
+        }
+    }
+
+    fn get(&mut self, key: i32) -> i32 {
+        if self.items.contains_key(&key) {
+            let position = self.order.iter().position(|&el| el == key).unwrap();
+            self.order.remove(position);
+            self.order.insert(0, key);
+
+            if self.order.len() > self.capacity as usize {
+                let popped = self.order.pop().unwrap();
+                self.items.remove(&popped);
+            }
+
+            return *self.items.get(&key).unwrap();
+        }
+
+        -1
+    }
+
+    fn put(&mut self, key: i32, value: i32) {
+        if let Some(existing_value) = self.items.get(&key) {
+            if existing_value != &value {
+                self.items.insert(key, value);
+            }
+
+            let position = self.order.iter().position(|&el| el == key).unwrap();
+            self.order.remove(position);
+            self.order.insert(0, key);
+
+            if self.order.len() > self.capacity as usize {
+                let popped = self.order.pop().unwrap();
+                self.items.remove(&popped);
+            }
+        } else {
+            self.items.insert(key, value);
+
+            self.order.insert(0, key);
+            if self.order.len() > self.capacity as usize {
+                let popped = self.order.pop().unwrap();
+                self.items.remove(&popped);
+            }
+        }
+    }
+}
+
+pub fn run() {
+    let mut cache = LRUCache::new(2);
+    cache.put(1, 1);
+    cache.put(2, 2);
+
+    println!("Get key 1 (expected 1): {}", cache.get(1));
+
+    cache.put(3, 3);
+    println!("Get key 2 (expected -1): {}", cache.get(2));
+
+    cache.put(4, 4);
+    println!("Get key 1 (expected -1): {}", cache.get(1));
+    println!("Get key 3 (expected 3): {}", cache.get(3));
+    println!("Get key 4 (expected 4): {}", cache.get(4));
+}
