9b40d097043d31ddc2ee91df1844bc59cd00356b
astra-kawa <mjagodzski@gmail.com>
2026-07-21 18:51:16 -0400

    minor cleanup

--- src/problems/lfu_cache.rs
+++ src/problems/lfu_cache.rs
@@ -1,7 +1,5 @@
-// https://leetcode.com/problems/lfu-cache
-#![allow(unused)]
 use core::fmt;
-use std::{collections::HashMap, println, writeln};
+use std::{collections::HashMap, println};
 
 #[derive(Debug)]
 struct Node {
@@ -30,7 +28,7 @@ impl fmt::Debug for FrequencyList {
             f,
             " Least: {:?} | Most: {:?}",
             self.least_recent, self.most_recent
-        );
+        )?;
         Ok(())
     }
 }
@@ -123,21 +121,6 @@ impl FrequencyList {
 
             self.nodes.insert(key, node);
             self.add_to_front(key);
-        }
-    }
-
-    fn get(&mut self, key: i32) -> i32 {
-        if self.nodes.contains_key(&key) {
-            if let Some(most_recent) = self.most_recent
-                && most_recent != key
-            {
-                self.detach(key);
-                self.add_to_front(key);
-            }
-
-            1
-        } else {
-            -1
         }
     }
 }
@@ -295,7 +278,7 @@ fn remove_brackets(test_case: &str) -> String {
 // example: [[2], [1, 1], [1], [2, 2], [2], [3, 3], [3], [4, 4], [4], [1], [1]]
 fn parse_and_run_test_case(test_case: &str) -> Vec<i32> {
     let mut result = Vec::new();
-    let mut ops = test_case.split("],").collect::<Vec<&str>>();
+    let ops = test_case.split("],").collect::<Vec<&str>>();
 
     let capacity = remove_brackets(ops.first().unwrap()).parse().unwrap();
     let mut cache = LFUCache::new(capacity);
@@ -318,25 +301,9 @@ fn parse_and_run_test_case(test_case: &str) -> Vec<i32> {
 }
 
 pub fn run() {
-    // let mut cache = LFUCache::new(2);
-
-    // cache.put(1, 1);
-    // cache.put(2, 2);
-    // println!("Get 1 (expected 1): {}", cache.get(1));
-
-    // cache.put(3, 3);
-    // println!("Get 2 (expected -1): {}", cache.get(2));
-    // println!("Get 3 (expected 3): {}", cache.get(3));
-
-    // cache.put(4, 4);
-    // println!("Get 1 (expected -1): {}", cache.get(1));
-
-    // println!("{:#?}", cache);
-
-    // test case:
     let test_case = "[[10],[10,13],[3,17],[6,11],[10,5],[9,10],[13],[2,19],[2],[3],[5,25],[8],[9,22],[5,5],[1,30],[11],[9,12],[7],[5],[8],[9],[4,30],[9,3],[9],[10],[10],[6,14],[3,1],[3],[10,11],[8],[2,14],[1],[5],[4],[11,4],[12,24],[5,18],[13],[7,23],[8],[12],[3,27],[2,12],[5],[2,9],[13,4],[8,18],[1,7],[6],[9,29],[8,21],[5],[6,30],[1,12],[10],[4,15],[7,22],[11,26],[8,17],[9,29],[5],[3,4],[11,30],[12],[4,29],[3],[9],[6],[3,4],[1],[10],[3,29],[10,28],[1,20],[11,13],[3],[3,12],[3,8],[10,9],[3,26],[8],[7],[5],[13,17],[2,27],[11,15],[12],[9,19],[2,15],[3,16],[1],[12,17],[9,1],[6,19],[4],[5],[5],[8,1],[11,7],[5,2],[9,28],[1],[2,2],[7,4],[4,22],[7,24],[9,26],[13,28],[11,26]]";
 
-    let result = parse_and_run_test_case(test_case);
+    parse_and_run_test_case(test_case);
 }
 
 #[cfg(test)]
