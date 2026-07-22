889865d8bcee264aee900de872d681086e2d167c
astra-kawa <mjagodzski@gmail.com>
2026-07-21 21:37:55 -0400

    naive working solution

--- src/problems/kth_largest_stream.rs
+++ src/problems/kth_largest_stream.rs
@@ -1,22 +1,53 @@
-use std::todo;
+use std::vec;
 
+#[derive(Debug)]
 struct KthLargest {
     k: i32,
-    largest_items: Vec<i32>,
-    largest: i32,
-    smallest: i32,
+    nums: Vec<i32>,
+    kth_largest: Option<i32>,
 }
 
 impl KthLargest {
     fn new(k: i32, nums: Vec<i32>) -> Self {
-        todo!()
+        let mut sorted = nums;
+        sorted.sort();
+
+        let start_idx = sorted.len().saturating_sub(k as usize);
+        let truncated = sorted.split_off(start_idx);
+
+        let kth_largest = truncated.first().copied();
+
+        KthLargest {
+            k,
+            nums: truncated,
+            kth_largest,
+        }
     }
 
-    fn add(&self, val: i32) -> i32 {
-        todo!()
+    fn add(&mut self, val: i32) -> i32 {
+        if self.nums.len() < self.k as usize
+            || self.kth_largest.is_some() && val > self.kth_largest.unwrap()
+        {
+            self.nums.push(val);
+
+            self.nums.sort();
+            let start_idx = self.nums.len().saturating_sub(self.k as usize);
+            self.nums = self.nums.split_off(start_idx);
+
+            self.kth_largest = self.nums.first().copied();
+        }
+
+        self.kth_largest.expect("Kth largest should exist")
     }
 }
 
 pub fn run() {
-    println!("Bruh");
+    let nums = vec![7, 7, 7, 7, 8, 3];
+    let mut kth = KthLargest::new(4, nums);
+
+    println!("Add 2: {}", kth.add(2));
+    println!("Add 10: {}", kth.add(10));
+    println!("Add 9: {}", kth.add(9));
+    println!("Add 9: {}", kth.add(9));
+    println!("{kth:?}");
 }
